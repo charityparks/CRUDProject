f177d466446961f6112699c9ddede6deaf9a637f
Charity Parks <tuppercharity@gmail.com>
2021-07-25 11:18:44 -0400

    firebase added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as firebase from 'firebase';
 
+var config = {
+  apiKey: "AIzaSyBN9WlmRc3SedmC4agM1G-rYqezGR22iZE"
+  authDomain: "crudproject-45834.firebase    app.com",
+  databaseURL: "https://crudproject-45834.firebaseio.com,"
+  projectId: "crudproject-45834",
+  storageBucket: "crudproject-45834.appspot.com",
+  messagingSenderId: "590481645308"
+  appId: "..."
+};
+firebase.initializeApp(Config);
 ReactDOM.render(
   <React.StrictMode>
     <App />
