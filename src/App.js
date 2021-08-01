9b478c2ea1cd03b020c924d09642e349ab03e7bf
Charity Parks <tuppercharity@gmail.com>
2021-08-01 11:24:15 -0400

    testing firebase

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import * as firebase from 'firebase';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor()
+  super ();
+  console.log(firebase);
+  render(){ 
+     return (
+        <div><h1> </h1>
+        </div>
+    
+      );
+  }
 }
 
 export default App;
